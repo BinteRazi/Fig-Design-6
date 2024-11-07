6a73f55058def8df26f45e4fed80756c32ec6a0e
Binte razi <binterazi086@gmail.com>
2024-11-07 20:22:19 +0500

    Class Assignment 4

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className='flex items-center justify-between p-4 w-[1920px] h-[134px] bg-[#A29875]'>
+      <div className="text-5xl font-normal text-[#FFFFFF] font-serif pl-4">MANZZARI
+      </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
